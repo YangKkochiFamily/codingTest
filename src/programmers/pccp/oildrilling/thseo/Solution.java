854a25cfc1c76e1fcfd9f19b48ee8c8ec5c061b6
STAY-TaeHui <masta95@naver.com>
2024-12-08 21:22:09 +0900

    [PCCP 기출문제] 2번 / 석유 시추 - 5번 실패. (BFS로 변경 필요)

--- src/programmers/pccp/oildrilling/thseo/Solution.java
+++ src/programmers/pccp/oildrilling/thseo/Solution.java
@@ -1,6 +1,6 @@
 package programmers.pccp.oildrilling.thseo;
 
-import java.util.Arrays;
+import java.util.*;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/250136
 public class Solution {
@@ -19,60 +19,68 @@ public class Solution {
     public int solution(int[][] land) {
 
         int maxOilCount = 0;
+
+        Map<Integer, Integer> oilBlockSizeMap = new HashMap<>();
+
+        calculateOilBlock(land, oilBlockSizeMap);
+
         for (int i = 0; i < land[0].length; i++) {//옆으로 이동
-            int [][] clonedLand = deppCopy(land);
             int oilCount = 0;
-
+            Set<Integer> keySet = new HashSet<>();
             for (int j = 0; j < land.length; j++) {
-                System.out.println(j +"-"+ i + " = " +land[j][i]);
-
-                if (clonedLand[j][i] == 0) {//석유 아님
-                    continue;
+                if (land[j][i] > 1) {
+                    keySet.add(land[j][i]);
                 }
-
-                oilCount += drilling(clonedLand, j, i);//석유 시추 시작
             }
+            oilCount = keySet.stream().mapToInt(oilBlockSizeMap::get).sum();
+
             maxOilCount = Math.max(maxOilCount, oilCount);
-            System.out.println("maxOilCount = " + maxOilCount);
         }
 
         return maxOilCount;
     }
 
-    private int[][] deppCopy(int[][] land) {
-        int [][] copy = new int[land.length][];
+    private void calculateOilBlock(int[][] land, Map<Integer, Integer> oilBlockSizeMap) {
+        int number = 2;
 
-        for (int i = 0; i < land.length; i++) {
-            copy[i] = Arrays.copyOf(land[i], land[i].length);
+        for (int i = 0; i < land[0].length; i++) {
+            int oilCount = 0;
+            for (int j = 0; j < land.length; j++) {
+                if (land[j][i] != 1) {//석유 아님
+                    continue;
+                }
+                oilCount = drilling(land, j, i, number);
+                oilBlockSizeMap.put(number,oilCount);
+                number++;
+            }
+
         }
-        return copy;
-
     }
 
-    private int drilling(int[][] clonedLand, int j, int i) {
+    private int drilling(int[][] clonedLand, int j, int i, int number) {
         int oilCount = 1;
 
         //뽑기 완료
-        clonedLand[j][i] = 0;
+        clonedLand[j][i] = number;
 
         //오른쪽 이동
         if (clonedLand[j].length - 1 > i && clonedLand[j][i+1] == 1) {
-            oilCount += drilling(clonedLand, j, i+1);
+            oilCount += drilling(clonedLand, j, i+1, number);
         }
 
         //아래로 이동
         if (clonedLand.length - 1 > j && clonedLand[j + 1][i] == 1) {
-            oilCount += drilling(clonedLand, j+1, i);
+            oilCount += drilling(clonedLand, j+1, i, number);
         }
 
         //왼쪽 이동
         if (0 < i && clonedLand[j][i-1] == 1) {
-            oilCount += drilling(clonedLand, j, i-1);
+            oilCount += drilling(clonedLand, j, i-1, number);
         }
 
         //위로 이동
         if (0 < j && clonedLand[j-1][i] == 1) {
-            oilCount += drilling(clonedLand, j-1, i);
+            oilCount += drilling(clonedLand, j-1, i, number);
         }
 
         return oilCount;
