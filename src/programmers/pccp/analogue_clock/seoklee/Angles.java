a97d8d488ae94beebf127cf83a6af9fcbfacb2be
lee-seokjun <72841771+lee-seokjun@users.noreply.github.com>
2025-01-02 07:23:01 +0900

    Create Angles.java

---
+++ src/programmers/pccp/analogue_clock/seoklee/Angles.java
@@ -0,0 +1,14 @@
+package programmers.pccp.analogue_clock.seoklee;
+
+public class Angles {
+    public double s = 0;
+    public double m = 0;
+    public double h = 0;
+
+    public Angles(double h, double m, double s) {
+        this.h = h;
+        this.m = m;
+        this.s = s;
+    }
+
+}
