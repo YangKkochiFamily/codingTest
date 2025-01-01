5682b70b16c6c57fca20d28a5884460b94a8515a
lee-seokjun <72841771+lee-seokjun@users.noreply.github.com>
2025-01-02 07:22:33 +0900

    Update Solution.java

--- src/programmers/pccp/analogue_clock/seoklee/Solution.java
+++ src/programmers/pccp/analogue_clock/seoklee/Solution.java
@@ -1,4 +1,19 @@
+package programmers.pccp.analogue_clock.seoklee;
+
 class Solution {
+    
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(0,5,30,0,7,0));
+        System.out.println(solution.solution(12,0,0,12,0,30));
+        System.out.println(solution.solution(0,6,1,0,6,6));
+        System.out.println(solution.solution(11,59,30,12,0,0));
+        System.out.println(solution.solution(11,58,59,11,59,0));
+        System.out.println(solution.solution(1,5,5,1,5,6));
+        System.out.println(solution.solution(0,0,0,23,59,59));
+    }
+    
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
         // 1초가 움직이면 분침은 얼마나 움직이는가? 
         // 60초 ->  6도 1초에 0.1도 움직임 
@@ -47,99 +62,3 @@ class Solution {
     }
 }
 
-class Time { 
-    public int h = 0;
-    public int m = 0;
-    public int s = 0;
-    public boolean isPm;
-    
-    public int mcnt = 0;
-    public int hcnt = 0;
-    
-    public Time (int h, int m, int s, boolean isPm) {
-        this.h = h;
-        this.m = m;
-        this.s = s;
-        this.mcnt = s;
-        this.hcnt = m * 60 + s;
-        this.isPm = isPm;
-    }
-
-    public String toString() {
-        return h + "시 " + m + "분 " + s + "초 ";
-    }
-    
-    public Angles getAngles() {
-        return new Angles(getAngleH(), getAngleM(), getAngleS());
-    }
-
-    public Angles getAfterAngles() {
-        return new Angles(getAngleH() == 0 ? 360 : getAngleH()
-                          , getAngleM() == 0 ? 360 : getAngleM()
-                          , getAngleS()== 0 ? 360 : getAngleS()
-                         );
-    }
-
-    public double getAngleH() {
-        return (double) (this.h * 30) + ((double) this.hcnt * (1.0 / 120));
-    }
-    
-    public double getAngleM() {
-        return (double) (this.m * 6) + ((double) this.mcnt * (1.0 / 10));
-    }
-    
-    public double getAngleS() {
-        return this.s * 6 ;
-    }
-    
-
-    public Time next() {
-        this.s++;
-        this.mcnt++;
-        this.hcnt++;
-        
-        if(this.mcnt == 60) {
-            m++;
-            this.mcnt = 0;
-        }
-        if(this.hcnt == 3600) {
-            h++;
-            this.hcnt = 0;
-        }
-        
-        if (s == 60) {
-            s = 0;
-        }
-        if (m == 60) {
-            m = 0;
-        }
-        if (h == 12) {
-            h = 0;
-            this.isPm = true;
-        }
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Time other = (Time) o;
-        return this.s == other.s 
-            && this.m == other.m 
-            && this.h == other.h 
-            && this.isPm == other.isPm;
-    }
-    
-}
-
-class Angles {
-    public double s = 0;
-    public double m = 0;
-    public double h = 0;
-    
-    public Angles(double h, double m, double s) {
-        this.h = h;
-        this.m = m;
-        this.s = s;
-    }
-    
-}
