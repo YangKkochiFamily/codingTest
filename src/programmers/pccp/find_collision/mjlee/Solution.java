400bbe87acb58fc702bbdb486af2f7b5c8269ab2
mjlee <mjlee@spectra.co.kr>
2025-01-02 19:39:45 +0900

    민주 아날로그 시계

---
+++ src/programmers/pccp/find_collision/mjlee/Solution.java
@@ -0,0 +1,17 @@
+package programmers.pccp.find_collision.mjlee;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int solution = s.solution(new int[][]{{3, 2}, {6, 4}, {4, 7}, {1, 4}}, new int[][]{{4, 2}, {1, 3}, {4, 2}, {4, 3}});
+    }
+
+    public int solution(int[][] points, int[][] routes) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
+
