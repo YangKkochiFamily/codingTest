ef3fd5e9e3fd9aae825d8e023821048ee84dffb7
seokjun <iope100@naver.com>
2025-02-15 16:18:24 +0900

    겉멋수정

--- python/seoklee/park.py
+++ python/seoklee/park.py
@@ -1,18 +1,10 @@
+from itertools import chain
 def solution(mats, park):
-  max_size = 0
-  for y, row in enumerate(park):
-    for x, value in enumerate(row):
-      if value == '-1':
-        if x > 0 and y > 0:
-          before_row = park[y - 1]
-          park[y][x] = min(row[x - 1], before_row[x], before_row[x - 1]) + 1
-          max_size = max(max_size, park[y][x])
-        else:
-          park[y][x] = 1
-      else:
-        park[y][x] = 0
-  return max(list(filter(lambda r: r <= max_size, mats + [-1])))
-
+  parsed_park = [[1 if '-1' == i else 0 for i in row] for row in park ]
+  for y, row in enumerate(parsed_park[1:], start = 1):
+    for x, value in [item for item in (enumerate(row[1:], start = 1)) if item[1] == 1]:
+      parsed_park[y][x] = min(row[x - 1], parsed_park[y - 1][x], parsed_park[y - 1][x - 1]) + 1
+  return max([r for r in mats + [-1] if r <= max(chain(*parsed_park))])
 
 if __name__ == '__main__':
   print(solution([1,5, 3, 2], [["A", "A", "-1", "B", "B", "B", "B", "-1"], ["A", "A", "-1", "B", "B", "B", "B", "-1"], ["-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"], ["D", "D", "-1", "-1", "-1", "-1", "E", "-1"], ["D", "D", "-1", "-1", "-1", "-1", "-1", "F"], ["D", "D", "-1", "-1", "-1", "-1", "E", "-1"]]))
