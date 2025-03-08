1b7594ac927df9c49c0b610402692c6c79931dc9
seokjun <iope100@naver.com>
2025-03-09 00:48:20 +0900

    설명추가

--- python/seoklee/perfect_clime.py
+++ python/seoklee/perfect_clime.py
@@ -1,4 +1,17 @@
 def solution(info, n, m):
+  """
+  완전탐색으로 해결할 수는 있지만 시간복잡도가 2^n 으로 너무 높다
+  다이나믹 프로그래밍 (dp) 으로 시간복잡도를 최적화 해보자.
+
+  point
+  1. 이전 결과를 저장 하는 memorization 사용
+    --  1번 아이템 훔칠 경우의 수 추가-> 1번 결과들 + 2번 아이템 훔치기 추가 ...
+  2. 경우의 수 중 목표 달성 못하는 경우는 memo에 추가 x
+  3. memo된 내용 중 b의 흔적이 동일한 경우 더 작은 a 만 저장
+    -- 복잡도는 b 최대 저장 가능 값  * 물건 갯수
+    -- O((b - 1) * len(info))
+  """
+
   # 초깃값을 설정해준다.
   # key : 해당 경우의 수의 b 값 총합
   # value : 해당 경우의 수의 a 총합
@@ -14,7 +27,7 @@ def solution(info, n, m):
       if b + B < m:
         tmp_memo[b + B] = min(a, tmp_memo.get(b + B, 121))
     memo = tmp_memo
-  return -1 if len(memo) == 0 else min(memo[b] for b  in memo)
+  return min(memo.values(), default=-1)
 
 if __name__ == '__main__':
   print(solution([[1, 2], [2, 3], [2, 1]],4,4))
