4b01f9ca6eb003abd9842d0f9c2f84bd9c631af4
STAY-TaeHui <masta95@naver.com>
2025-03-12 23:34:10 +0900

    [2025 프로그래머스 코드챌린지 1차 예선] - 완전범죄 문제 이해가 어려운걸요,,,

---
+++ src/programmers/code_challenge/perfect_crime/thseo/Solution.java
@@ -0,0 +1,8 @@
+package programmers.code_challenge.perfect_crime.thseo;
+
+public class Solution {
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+        return answer;
+    }
+}
